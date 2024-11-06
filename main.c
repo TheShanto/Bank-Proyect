09119a05fe52d1fbb9bfbb20db0ce60ea1ca9bff
Pedro Remigio Bracamontes <88297357+TheShanto@users.noreply.github.com>
2024-11-06 14:46:52 -0600

    Update main.c
    Se agregaron las funciones principales del Código (Sin argumentos o logica interna)

--- main.c
+++ main.c
@@ -2,9 +2,47 @@
 #include<stdio.h>
 #include<math.h>
 
-main(){
-printf("ñ");
+// Menú principal
+void StartMenu(){
+  
+}
 
-getch ()
-return 0
+// Funcion para crear cuentas
+int SignUpFunction(){
+  
+}
+
+// Funcion para agregar dinero a la cuenta
+int AddMoneyFunction(){
+  
+}
+
+// Funcion para ver el dinero de la cuenta
+int ViewMoneyFunction(){
+  
+}
+
+// Funcion para transferir dinero
+int TransferMoney(){
+  
+}
+
+// Funcion para agregar saldo a un número de telefono
+int AirTimeRecharge(){
+  
+}
+
+// Funcion para cambiar el NIP
+int NipChange(){
+  
+}
+
+// Funcion para salir de la cuenta del usuario
+int LogOutFunction(){
+  
+}
+
+// Arranque del programa
+main(){
+  
 }
